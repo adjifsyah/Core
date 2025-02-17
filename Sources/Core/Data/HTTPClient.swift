300886fb568610960737e692ad57d91a7ab9c5d1
Apple Josal <adji.firmansyah27@yahoo.com>
2025-02-17 17:10:44 +0700

    DEV - Add NetworkConfiguration

--- Sources/Core/Data/HTTPClient.swift
+++ Sources/Core/Data/HTTPClient.swift
@@ -11,3 +11,20 @@ import RxSwift
 protocol HttpClient {
     func load(url: URL, method: String, params: [String: String]?) -> Observable<Data>
 }
+
+class NetworkConfiguration {
+    static let shared: ((String, String) -> NetworkConfiguration) = { host, apiKey in
+        NetworkConfiguration(host: host, apiKey: apiKey)
+    }
+    
+    public init(
+        host: String,
+        apiKey: String
+    ) {
+        self.host = host
+        self.apiKey = apiKey
+    }
+    
+    var host: String
+    var apiKey: String
+}
