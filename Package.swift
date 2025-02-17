bbead824ab889c9e38e1e03fb0f67f15f09a3727
Apple Josal <adji.firmansyah27@yahoo.com>
2025-02-17 10:03:35 +0700

    Add framework

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Core",
+    platforms: [.iOS(.v16)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Core",
+            targets: ["Core"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.0"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Core",
+            dependencies: [
+                .product(name: "RxCocoa", package: "RxSwift"),
+                .product(name: "Alamofire", package: "Alamofire")
+            ]
+        ),
+        .testTarget(
+            name: "CoreTests",
+            dependencies: ["Core"]),
+    ]
+)
