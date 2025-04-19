4cabe60cccfe1309d73bbc87e17550a847820e86
Apple Josal <adji.firmansyah27@yahoo.com>
2025-04-19 20:25:06 +0700

    change request name entity

--- Sources/Core/Data/LocaleDataSource.swift
+++ Sources/Core/Data/LocaleDataSource.swift
@@ -13,7 +13,7 @@ public protocol LocaleDataSource {
     associatedtype Response
     
     func list(request: Request?) -> Observable<[Response]>
-    func add(entities: [Response]) -> Observable<Bool>
+    func add(entity: Response) -> Observable<Bool>
     func update(id: Int, entity: Response) -> Observable<Bool>
     func delete(id: Int) -> Observable<Bool>
 }
